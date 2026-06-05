87cca1065ed2579eb88b2b020952136b31199a8e
Juan Manuel Tierno <jtendersen@hotmail.com>
2026-06-05 19:12:10 -0300

    Initial Spring Boot project

---
+++ src/test/java/ar/edu/utn/desi/hogarya/HogaryaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.desi.hogarya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogaryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
